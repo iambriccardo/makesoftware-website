8a39f645f96e05b08046bd0ee3e3d0b464fd9e99
Riccardo Busetti <riccardob36@gmail.com>
2026-06-14 15:57:35 +0200

    Improve

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import react from "@vitejs/plugin-react";
 const rootDir = process.cwd();
 const staticEntries = [
   "assets",
-  "fonts",
   "site.webmanifest",
   "old-index.html",
   "new-brand.html",
